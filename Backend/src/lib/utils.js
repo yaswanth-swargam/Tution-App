b4154c5e09e7764d6ddffe9e17ee4d169565641e
Swargam Yaswanth <223908837+yaswanth-swargam@users.noreply.github.com>
2026-09-14 17:33:47 +0530

    added

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -1,17 +1,22 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const generateToken=(userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:'7d'
-    })
+const generateToken = (userId, res) => {
+    const token = jwt.sign(
+        { userId },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d",
+        }
+    );
 
-    res.cookie('jwt', token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    secure: process.env.NODE_ENV === 'production'
-})
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
+
     return token;
-}
+};
 
 export default generateToken;
