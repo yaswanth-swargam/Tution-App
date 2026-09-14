7daa3a9b11a2005f1801937d6c55f4b2fc6e1a43
Swargam Yaswanth <223908837+yaswanth-swargam@users.noreply.github.com>
2026-09-14 16:51:15 +0530

    cleared

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {signup,signin,logout,updateProfile,checkAuth,bootstrapAdmin, createUser} from '../controllers/auth.controller.js'
+import {signin,logout,updateProfile,checkAuth, createUser} from '../controllers/auth.controller.js'
 import protectRoute from '../middleware/auth.middleware.js'
 import adminOnly from '../middleware/admin.middleware.js'
 const router=express.Router()
